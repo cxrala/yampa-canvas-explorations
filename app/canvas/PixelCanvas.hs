3c27e05a7077c52ac9ce66455170d778d56507c1
cxrala <ameliang123@gmail.com>
2023-12-13 14:46:38 +0000

    made the vector operations a bit clearner

--- app/canvas/PixelCanvas.hs
+++ app/canvas/PixelCanvas.hs
@@ -1,11 +1,14 @@
 module PixelCanvas where
 
 import Data.Word (Word8)
-import qualified Data.Vector.Unboxed as VU
-import Graphics.Blank hiding (Event)
 import Data.Point2
 import qualified Data.Vector.Unboxed.Mutable as MVU
+import qualified Data.Vector.Unboxed as VU
+import Data.Vector.Unboxed.Mutable (MVector)
+import Data.Vector.Unboxed (Vector)
 import GHC.Float.RealFracMethods
+import Control.Monad.ST
+import Graphics.Blank hiding (Event)
 
 
 data PixelCanvas = PixelCanvas {
@@ -15,35 +18,40 @@ data PixelCanvas = PixelCanvas {
 
 data ColouredPixel = ColouredPixel {
     -- values constrained from 0 to 255
-    r       :: Int,
-    g       :: Int,
-    b       :: Int,
-    a       :: Int
+    r       :: Word8,
+    g       :: Word8,
+    b       :: Word8,
+    a       :: Word8
 } deriving (Show)
 
 
 -- builds an empty ImageData (ie an empty buffer)
 canvasBuilder :: PixelCanvas -> ImageData
 canvasBuilder PixelCanvas{height, width} = 
-    ImageData height width $ replicate (h * w * 4) (fromIntegral 0)
+    ImageData height width $ VU.replicate (h * w * 4) (fromIntegral 0)
 
 -- 2d coordinate to row-major rgba index START point (stride is 4)
 vecIndexFromPoint :: Point2 Double -> Int -> Int
 vecIndexFromPoint (Point2 x y) width =
     let stride = 4 in (floorDoubleInt x) * stride + (floorDoubleInt y) * stride * width
 
--- gives you a function that will write the next three elements from index i with the coloured pixel values
-writeColouredPixel :: ColouredPixel -> Int -> VU.Vector Word8 -> VU.Vector Word8
-writeColouredPixel ColouredPixel{r, g, b, a} i vec =
-    let modify idx val prev = VU.modify (\v -> MVU.write v idx (fromIntegral val)) prev
-    in  modify (i + 3) a $ modify (i + 2) b $ modify (i + 1) g $ modify i r vec
-    -- let vr = VU.modify (\v -> MVU.write v i (fromIntegral r)) vec
-    -- in let vg = VU.modify (\v -> MVU.write v (i + 1) (fromIntegral g)) vr
-    -- in let vb = VU.modify (\v -> MVU.write v (i + 2) (fromIntegral b)) vg
-    -- in VU.modify (\v -> MVU.write v (i + 3) (fromIntegral a)) vb
+-- runs op on a vector, giving you a new vector. this is safe (but O(n))
+inPlace :: (forall s. MVector (PrimState (ST s)) a -> ST s ()) -> Vector a -> Vector a
+inPlace op vec = runST $ do
+    mv <- VU.thaw vec
+    op mv
+    V.freeze mv
+
+-- writes a coloured pixel into a mutable vector at index i
+writeColouredPixel :: ColouredPixel -> Int -> MVector (PrimState (ST s)) a -> ST s ()
+writeColouredPixel pixel i mvec = do
+    MVU.write mvec i $ r pixel
+    MVU.write mvec (i + 1) $ g pixel
+    MVU.write mvec (i + 2) $ b pixel
+    MVU.write mvec (i + 3) $ a pixel
 
 -- sets a value at a specific coordinate to a pixel
 setPixel :: ColouredPixel -> Point2 Double -> ImageData -> ImageData
 setPixel pixel coord (ImageData w h d) =
-    let v = writeColouredPixel pixel (vecIndexFromPoint coord) d in
+    let v = inPlace $ writeColouredPixel pixel vecIndexFromPoint coord $ d in
     ImageData w h v
